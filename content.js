b5ba0e369a94af05838a84ea9154e5baa2b25e50
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2019-09-05 18:56:34 -0300

    init

---
+++ content.js
@@ -0,0 +1,28 @@
+console.info('Airflow lifUnf is activated')
+
+AUTO_REFRESH_HASH = '#auto-refresh'
+
+isAutoRefreshEnabled = ()=>{
+    return location.hash.indexOf(AUTO_REFRESH_HASH) != -1
+}
+
+btnTitle = isAutoRefreshEnabled()? 'Turn Off': 'Auto Refresh'
+btnHref = isAutoRefreshEnabled()? '': '#auto-refresh'
+autoRefreshButton = '<li>'
+autoRefreshButton +=   '<a id="auto_refresh" href="'+btnHref+'">'
+autoRefreshButton +=   '<span class="glyphicon glyphicon-repeat" aria-hidden="true" data-original-title="" title="">'
+autoRefreshButton +=   '</span> '+btnTitle+'</a>'
+autoRefreshButton +='</li>'
+$('ul.nav.nav-pills').append(autoRefreshButton)
+
+setInterval(()=>{
+    if (isAutoRefreshEnabled())
+        location.reload()
+}, 1500);
+
+function createAutoRefreshInterval() {
+  return setInterval(()=>{
+  if (isAutoRefreshEnabled())
+      location.reload()
+  }, 1500);
+}
