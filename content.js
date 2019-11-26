b8bcf81597796251d3963e680a0c063a56223e25
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2019-11-26 19:23:20 -0300

    add link to logs on databriks

--- content.js
+++ content.js
@@ -18,11 +18,35 @@ $('ul.nav.nav-pills').append(autoRefreshButton)
 setInterval(()=>{
     if (isAutoRefreshEnabled())
         location.reload()
-}, 1500);
+}, 5000);
 
-function createAutoRefreshInterval() {
-  return setInterval(()=>{
-  if (isAutoRefreshEnabled())
-      location.reload()
-  }, 1500);
+let createAutoRefreshInterval = () => setInterval(
+    ()=>{
+      if (isAutoRefreshEnabled())
+          location.reload()
+    }, 1500);
+
+$(document).ready(()=>{
+    apply_links_on_logs_page()
+})
+
+let apply_links_on_logs_page = ()=>{
+    if (window.location.href.indexOf('/admin/airflow/log') != -1) {
+        log_text = ''
+
+        // wait content to be loaded
+        let getTextInterval = (() => setInterval(
+            () => {
+              if (log_text != ''){
+                $('body > .container .tab-pane.active pre code').html(urlify(log_text))
+                clearInterval(getTextInterval)
+              }
+              log_text = $('body > .container .tab-pane.active pre code').html()
+            }, 300))();
+    }
+}
+
+let urlify = (text)=> {
+    let urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(urlRegex, (url) => '<a href="' + url + '" target="blank">' + url + '</a>')
 }
